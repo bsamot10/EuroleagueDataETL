1a4abf3232aa1758312a9aeca756ee955425d4d1
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2022-11-28 14:13:44 +0200

    Update scrapper.py
    update comments

--- euroleague_data_loader/scrapper.py
+++ euroleague_data_loader/scrapper.py
@@ -18,39 +18,39 @@ class EuroScrapas:
         This is the main instance method of the class. 
         It makes use of the remaining instance methods and provides a step by step implementation of the process.
         '''  
-        # IMPLEMENT PROCESS PER SEASON
+        # implement process per season 
         for index, sc in enumerate(self.season_codes):
 
-            # INITIALIZE BASIC VARIABLES OF PROCESS
+            # initialize basic variable of process
             process_start_time, season_end, meta_data_dict, gc = self.initialize_process(index, sc)
 
             while not season_end:
 
-                # UPDATE THE GAME_CODE COUNTER
+                # update the game_code counter
                 gc += 1 
 
-                # SCRAP APIs, LOAD DATA AND UPDATE INFO
+                # scrap APIs, load data and update info
                 meta_data_dict = self.request_extract_load_update(gc, sc, process_start_time, meta_data_dict)
 
-                # SAVE META_DATA
+                # save meta_data
                 with open(fr"euroleague_data/{sc}/{sc}_meta_data_{self.datetime_now}.json", "w") as output_file:
                     json.dump(meta_data_dict, output_file)
 
-                # UPDATE FLAG --- THE 1st CONDITION IS OBVIOUS --- THE 2nd CONDITION REFLECTS A POSSIBLE PROBLEM WITH THE URLs OR A PREMATURE END OF THE SEASON (e.g. COVID SEASON E2019)
+                # update flag --- the 1st condition is obvious --- the 2nd condition reflects a possible problem with the URLs or a premature end of the season (e.g. covid season E2019)
                 if (meta_data_dict[sc]["number_of_FinalFour_games"] == 4) or (meta_data_dict[sc]["number_of_failed_extractions"] > self.failed_extractions_limit):
                     season_end = True
         
     def initialize_process(self, index, sc):
         
-        # INITIALIZE TIMING & FlAG 
+        # initialize timing & flag 
         process_start_time = time()
         season_end = False
 
-        # ONLY THE FIRST SEASON_CODE CAN START WITH GAME_CODE > 1
+        # only the first season_code can start with game_code > 1 
         if index > 0:
             self.game_code_start = 1
 
-        # INITIALIZE THE META_DATA DICTIONARY
+        # initialize the meta_data dictionary
         meta_data_dict = {sc: {"total_number_of_games": 0,
                                "number_of_RegularSeason_games": 0,
                                "number_of_Top16_games": 0,
@@ -63,12 +63,12 @@ class EuroScrapas:
                                "unknown_Phase": [],
                                "time_counter": None}}
 
-        # CREATE DIRECTORIES
+        # create directories
         if not os.path.exists(fr"euroleague_data/{sc}"):
             os.makedirs(fr"euroleague_data/{sc}/success")
             os.makedirs(fr"euroleague_data/{sc}/failure")
 
-        # INITIALIZE THE GAME_CODE COUNTER
+        # initialize the game_code counter
         gc = self.game_code_start - 1
         
         return process_start_time, season_end, meta_data_dict, gc
@@ -77,36 +77,36 @@ class EuroScrapas:
                   
         for api in self.euroleague_apis:
 
-            # HTTP REQUEST
+            # HTTP request
             url = f"https://live.euroleague.net/api/{api}?gamecode={str(gc)}&seasoncode={sc}"
             response = requests.get(url)
             response_status = response.status_code
 
             try:
-                # EXTRACT DATA
+                # extract data
                 response_dict = response.json()
 
-                # GET USEFUL INFO FROM HEADER API
+                # get useful info from Header API
                 if api == "Header":
                     Phase, Round, Date, meta_data_dict = self.get_info_from_header_api(response_dict, meta_data_dict, url, gc, sc, process_start_time)
 
-                # SET THE FILENAME OF THE JSON FILE
+                # set the filename of the json file
                 json_filename = "_".join([sc, Phase, Round, "{:02d}".format(gc), Date, api])
 
-                # LOAD DATA
+                # load data
                 with open(fr"euroleague_data/{sc}/success/{json_filename}.json", "w") as output_file:
                     json.dump(response_dict, output_file)
 
             except Exception as e:
                 print(e, "--- URL:", url)
-                # UPDATE THE FAILURE COUNTER OF META_DATA DICTIONARY
+                # udpate the failure counter of the meta_data dictionary
                 meta_data_dict[sc]["number_of_failed_extractions"] += 1
-                # UDPATE THE FAILURE FILE BY SAVING THE FAILED URL AND THE STATUS CODE OF THE RESPONSE)
+                # update the failure file by saving the failed URL and the status_code of the responce
                 with open(fr"euroleague_data/{sc}/failure/{sc}_failed_extractions_{self.datetime_now}.txt", 'a') as failure_file:
                     failure_file.write(f"failed_url: {url}  ---  status_code: {str(response_status)}\n")
                     failure_file.close()
                     
-            # UPDATE THE TIME AND GAME_CODE COUNTERS OF META_DATA DICTIONARY
+            # update the time and game_code counters of the meta_data dictionary
             meta_data_dict[sc]["time_counter"] = f"{round((time() - process_start_time) / 60, 1)} minutes"
             meta_data_dict[sc]["game_code_counter"] = gc
                     
@@ -114,7 +114,7 @@ class EuroScrapas:
     
     def get_info_from_header_api(self, response_dict, meta_data_dict, url, gc, sc, process_start_time):
     
-        # GET USEFUL INFO FROM HEADER API
+        # get useful info from Header API
         Round = "{:02d}".format(int(response_dict["Round"]))
         Date_split = response_dict["Date"].split("/")
         Date = "".join([Date_split[2], Date_split[1], Date_split[0]])
@@ -124,7 +124,7 @@ class EuroScrapas:
         else:
             Phase = Phase_split[0].capitalize()
 
-        # UPDATE THE GAME COUNTERS OF META_DATA DICTIONARY    
+        # update the game counters of the meta_data dictionary    
         meta_data_dict[sc]["total_number_of_games"] += 1
         if Phase.startswith("Reg"):
             meta_data_dict[sc]["number_of_RegularSeason_games"] += 1
@@ -137,7 +137,7 @@ class EuroScrapas:
         else:
             meta_data_dict[sc]["unknown_Phase"].append([Phase, url])
 
-        # PRINT A MESSAGE FOR MONITORING PURPOSES
+        # print a message for monitoring purposes
         if gc == 1:
             print("")
         print(f"SeasonCode: {sc}  ---  Phase: {Phase}  ---  Round: {Round}  ---  GameCode:", "{:02d}".format(gc), \
